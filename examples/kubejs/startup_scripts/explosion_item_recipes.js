36809994c437f9e70cab53ffaec0df2730d7b40f
li_waiwai <2157995671a@gmail.com>
2026-06-14 02:19:14 +0800

    1

--- examples/kubejs/startup_scripts/explosion_item_recipes.js
+++ examples/kubejs/startup_scripts/explosion_item_recipes.js
@@ -1,16 +1,26 @@
 FlukeEvents.explosionRecipes(event => {
 
-  event.add('test_1', [
+  event.add('minecraft:raw_iron', [
     { item: 'minecraft:iron_nugget', count: 3, chance: 1.0 },
     { item: 'minecraft:iron_ingot', count: 1, chance: 0.25 }
   ])
 
   event.add({
-    id: 'test_2',
+    id: 'test_1',
     input: 'minecraft:coal',
     outputs: [
       { item: 'minecraft:diamond', count: 1, chance: 0.05 },
       { item: 'minecraft:charcoal', count: 1, chance: 0.5 }
     ]
   })
+
+  event.add({
+    id: 'test_2',
+    input: '3 minecraft:coal_block',
+    outputs: [
+      { item: 'minecraft:diamond', count: 1, chance: 0.25 }
+    ]
+  })
+
 })
+
